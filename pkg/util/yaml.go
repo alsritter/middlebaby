a60d1ccce9177a390a1eea941bc99d1494b5a44f
alsritter <alsritter@outlook.com>
2022-09-10 23:05:56 +0800

    加上版权声明

--- pkg/util/yaml.go
+++ pkg/util/yaml.go
@@ -1,9 +1,27 @@
+/*
+ Copyright (C) 2022 alsritter
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package util
 
 import (
+	"io/ioutil"
+
 	"github.com/hashicorp/go-multierror"
 	"gopkg.in/yaml.v2"
-	"io/ioutil"
 )
 
 // LoadConfig read YAML-formatted config from filename into cfg.
